747c939495cd1a0b750608dc0bb631bb86fbf1a9
Rahul Sharma <developerrahul671@gmail.com>
2023-03-21 11:37:31 +0530

    Add files via upload

--- DoublyLinkedList.java
+++ DoublyLinkedList.java
@@ -9,8 +9,8 @@ public class DoublyLinkedList {
         private ListNode next;
         private ListNode previous;
 
-        public ListNode(){
-            this.data = data;
+        public ListNode(int val){
+            this.data = val;
 
         }
     }
@@ -26,5 +26,52 @@ public class DoublyLinkedList {
     }
     public int length() {
         return length;
+    }
+
+    public void insertLast(int value){
+        ListNode newNode = new ListNode(value);
+        if (isEmpty()){
+            head = newNode;
+        } else {
+            tail.next = newNode;
+        }
+        newNode.previous = tail;
+        tail = newNode;
+        length++;
+    }
+
+    public void displayForward(){
+        if (head == null){
+            return;
+        }
+        ListNode temp = head;
+        while (temp != null){
+            System.out.print(temp.data+"-->");
+            temp = temp.next;
+        }
+        System.out.println("null");
+    }
+
+    public void displayBackward(){
+        if (tail == null){
+            return;
+        }
+        ListNode temp = tail;
+        while (temp != null){
+            System.out.print(temp.data+"-->");
+            temp = temp.previous;
+        }
+        System.out.println("null");
+    }
+
+    public static void main(String[] args) {
+        DoublyLinkedList dll = new DoublyLinkedList();
+        dll.insertLast(1);
+        dll.insertLast(10);
+        dll.insertLast(15);
+        dll.insertLast(19);
+
+        dll.displayForward();
+        dll.displayBackward();
     }
 }
